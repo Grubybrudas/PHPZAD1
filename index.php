99abe41d80a7b09112115bc88a8fbfb9d639e811
Grubybrudas <grubybrudas1x@gmail.com>
2026-09-14 13:29:05 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php 
+    $a = 18;
+    echo $a;
+
+    echo "<br/>";
+
+    echo $a - $a * 2;
+
+    echo "<br/>";
+
+    echo $a + 3;
+
+    echo "<br/>";
+
+    echo $a - 5;
+
+    echo "<br/>";
+
+    echo $a * 2;
+
+    echo "<br/>";
+
+    echo $a / 5;
+
+    echo "<br/>";
+
+    echo $a % 2;
+
+    echo "<br/>";
+
+    echo $a * $a * $a;
+
+    echo "<br/>";
+
+    echo "Wartość zmiennej $a wynosi " . $a . " i już się nie zmieni";
+    ?>
+    
+</body>
+</html>
